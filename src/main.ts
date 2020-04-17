c1d4ea56b81106581713eaa13b0f88de33f25a77
Remi Arquevaux <r.arquevaux@gmail.com>
2020-04-17 12:16:13 +0200

    add docker and docker-compose

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
+import { Transport, MicroserviceOptions } from '@nestjs/microservices';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.createMicroservice(MicroserviceOptions)AppModule, {
+    transport: Transport.GRPC,
+    },
+  );
+  app.listen(() => console.log('Microservice is listening'));
 }
 bootstrap();
