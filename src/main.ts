ca37d55a935e83de90a0ecedabd4bc796f076355
Remi Arquevaux <r.arquevaux@gmail.com>
2020-04-17 13:39:22 +0200

    create ms with TCP

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { Transport, MicroserviceOptions } from '@nestjs/microservices';
 import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 async function bootstrap() {
-  const app = await NestFactory.createMicroservice(MicroserviceOptions)AppModule, {
-    transport: Transport.GRPC,
-    },
-  );
+  const app = await NestFactory.createMicroservice(AppModule, {
+    transport: Transport.TCP,
+    options: {
+      host: "127.0.0.1",
+      port: 3001
+    }
+  });
+
   app.listen(() => console.log('Microservice is listening'));
 }
 bootstrap();
